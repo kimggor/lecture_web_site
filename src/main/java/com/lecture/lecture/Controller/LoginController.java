a104560fd19dcd53978dfb22eaa484894e75fa4a
kimggor <dkdjsema9808@naver.com>
2025-01-20 17:09:02 +0900

    /매핑 추가

--- src/main/java/com/lecture/lecture/Controller/LoginController.java
+++ src/main/java/com/lecture/lecture/Controller/LoginController.java
@@ -3,6 +3,8 @@ package com.lecture.lecture.Controller;
 import com.lecture.lecture.Service.StudentService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,19 @@ public class LoginController {
 
     @Autowired
     private StudentService studentService;
+
+    @GetMapping("/")
+    public String home() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()
+                && !(authentication.getPrincipal() instanceof String
+                && authentication.getPrincipal().equals("anonymousUser"))) {
+            // 사용자가 인증된 경우
+            return "redirect:/enrollment";
+        }
+        // 사용자가 인증되지 않은 경우
+        return "redirect:/login";
+    }
 
     // 로그인 페이지 표시
     @GetMapping("/login")
